dfd554daa246fdb3a1282b4cc368af691183964a
redbearder <redbearder@gmail.com>
2019-10-15 01:45:00 +0800

    strict test

--- src/backend/app/email.py
+++ src/backend/app/email.py
@@ -11,8 +11,13 @@ def send_async_email(app, msg):
         mail.send(msg)
 
 
-def send_email(subject, sender, recipients, text_body, html_body,
-               attachments=None, sync=False):
+def send_email(subject,
+               sender,
+               recipients,
+               text_body,
+               html_body,
+               attachments=None,
+               sync=False):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
@@ -23,4 +28,4 @@ def send_email(subject, sender, recipients, text_body, html_body,
         mail.send(msg)
     else:
         Thread(target=send_async_email,
-            args=(current_app._get_current_object(), msg)).start()
+               args=(current_app._get_current_object(), msg)).start()
