03e01ee5e4e4d8ecce36d7a479294833a47c4b3a
redbearder <redbearder@gmail.com>
2019-10-23 16:00:47 +0800

    change backend port to 22330

--- src/backend/boot.sh
+++ src/backend/boot.sh
@@ -10,4 +10,4 @@ while true; do
     sleep 5
 done
 flask translate compile
-exec gunicorn -b :5000 --access-logfile - --error-logfile - microblog:app
+exec gunicorn -b :22330 --access-logfile - --error-logfile - sapspa:app
