9b3aa0ae716e59494b8803ad5147329e5081a104
redbearder <redbearder@gmail.com>
2020-02-27 17:03:27 +0800

    update backend requirements update start script with functions

--- script/start_agent.sh
+++ script/start_agent.sh
@@ -73,61 +73,114 @@ do
         esac
 done
 
-# download pyenv
-echo "download pyenv"
-wget https://github.com/pyenv/pyenv/archive/v${PYENV_VERSION}.tar.gz -O ${BASE_DIR}script/download/pyenv.tar.gz
-echo "install pyenv"
-tar zxvf ${BASE_DIR}script/download/pyenv.tar.gz ${BASE_DIR}script/download/
-mv ${BASE_DIR}script/download/pyenv-${PYENV_VERSION} ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
-exec "$SHELL"
-# install python3.7.5 via pyenv
-mkdir ~/.pyenv/sources
-mkdir ~/.pyenv/sources/${PYTHON_VERSION}
-echo "download Python "$PYTHON_VERSION
-wget https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tar.xz ~/.pyenv/sources/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tar.xz
-echo "install Python "${PYTHON_VERSION}
-pyenv install ${PYTHON_VERSION}
-pyenv local ${PYTHON_VERSION}
-# pip install -r requirements.txt
-echo "Install Python requirements"
-pip install -r ${BASE_DIR}src/agent/requirements.txt
-# install nwrfc lib
-echo "install nwrfc lib"
-unzip ${BASE_DIR}lib/nwrfc750P_4-70002752.zip -d ${BASE_DIR}script/download/
-mv ${BASE_DIR}script/download/nwrfcsdk /usr/sap/nwrfcsdk
-touch /etc/ld.so.conf.d/nwrfcsdk.conf
-echo "# include nwrfcsdk" >> /etc/ld.so.conf.d/nwrfcsdk.conf
-echo "/usr/sap/nwrfcsdk/lib" >> /etc/ld.so.conf.d/nwrfcsdk.conf
-ldconfig
-# install pyrfc module
-echo "install pyrfc module"
-pip install ${BASE_DIR}lib/pyrfc-1.9.98-cp37-cp37m-linux_x86_64.whl
-# download consul
-echo "download consul"
-wget https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip -O ${BASE_DIR}script/download/consul.zip
-unzip ${BASE_DIR}script/download/consul.zip -d /usr/local/bin
-# start consul
-echo "start consul"
-nohup consul agent -data-dir=${BASE_DIR}data/consul -ui -client=0.0.0.0 -bind=0.0.0.0 -join=${MASTER_IP} -dns-port=23346 -http-port=23345 -retry-join "[::1]:23341" -serf-wan-port=23342 &
-# start sapspa_agent.py
-echo "start sapspa_agent.py"
-uwsgi --http 0.0.0.0:23310 --wsgi-file ${BASE_DIR}src/agent/sapspa_agent.py --callable app_dispatch --daemonize /var/log/uwsgi_sapspa_agent.log
-# download node_exporter
-echo "download node_exporter"
-wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz -O ${BASE_DIR}script/download/node_exporter.tar.gz
-tar zxvf ${BASE_DIR}script/download/node_exporter.tar.gz /usr/local/bin
-# start node_exporter
-echo "start node_exporter"
-nohup node_exporter --web.listen-address=":23311" &
-# download ELK filebeat
-wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-${CONSUL_VERSION}-linux-x86_64.tar.gz -O ${BASE_DIR}script/download/filebeat.tar.gz
-tar zxvf ${BASE_DIR}script/download/filebeat.tar.gz ${BASE_DIR}script/download/
-mv ${BASE_DIR}script/download/filebeat-${ELK_VERSION}-linux-x86_64 ${BASE_DIR}app/filebeat
-# start filebeat
-sed -i "s/localhost:23392/${MASTER_IP}:23392/g" ${BASE_DIR}etc/filebeat/filebeat.yml
-nohup ${BASE_DIR}app/filebeat/filebeat -c ${BASE_DIR}etc/filebeat/filebeat.yml &
-echo "start agent done"
+function install_pyenv()
+{
+        if command -v pyenv 1> /dev/null 2>&1; then
+            echo "pyenv already exist"
+        else
+            # download pyenv
+            echo "download pyenv"
+            wget https://github.com/pyenv/pyenv/archive/v${PYENV_VERSION}.tar.gz -O ${BASE_DIR}script/download/pyenv.tar.gz
+            echo "install pyenv"
+            tar zxvf ${BASE_DIR}script/download/pyenv.tar.gz ${BASE_DIR}script/download/
+            mv ${BASE_DIR}script/download/pyenv-${PYENV_VERSION} ~/.pyenv
+            echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+            echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+            echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
+            exec "$SHELL"
+        fi
+}
 
+function install_python3()
+{
+        if command -v python3 1> /dev/null 2>&1; then
+            echo "python3 already exist"
+        else
+            # install python3.7.5 via pyenv
+            mkdir ~/.pyenv/sources
+            mkdir ~/.pyenv/sources/${PYTHON_VERSION}
+            echo "download Python "$PYTHON_VERSION
+            wget https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tar.xz ~/.pyenv/sources/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tar.xz
+            echo "install Python "${PYTHON_VERSION}
+            pyenv install ${PYTHON_VERSION}
+            pyenv local ${PYTHON_VERSION}
+        fi
+}
+
+function install_agent_requirements()
+{
+  # pip install -r requirements.txt
+  echo "Install Python requirements"
+  pip install -r ${BASE_DIR}src/agent/requirements.txt
+}
+
+
+function install_pyrfc()
+{
+  # install nwrfc lib
+  echo "install nwrfc lib"
+  unzip ${BASE_DIR}lib/nwrfc750P_4-70002752.zip -d ${BASE_DIR}script/download/
+  mv ${BASE_DIR}script/download/nwrfcsdk /usr/sap/nwrfcsdk
+  touch /etc/ld.so.conf.d/nwrfcsdk.conf
+  echo "# include nwrfcsdk" > /etc/ld.so.conf.d/nwrfcsdk.conf
+  echo "/usr/sap/nwrfcsdk/lib" >> /etc/ld.so.conf.d/nwrfcsdk.conf
+  ldconfig
+  # install pyrfc module
+  echo "install pyrfc module"
+  pip install ${BASE_DIR}lib/pyrfc-1.9.98-cp37-cp37m-linux_x86_64.whl
+}
+
+function install_consul()
+{
+  # download consul
+  echo "download consul"
+  wget https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip -O ${BASE_DIR}script/download/consul.zip
+  unzip ${BASE_DIR}script/download/consul.zip -d /usr/local/bin
+  # start consul
+  # if process consul exist and ignore
+  count=`ps -ef |grep consul |grep -v "grep" |wc -l`
+  if [ 0 == $count ];then
+        echo "start consul"
+        nohup consul agent -data-dir=${BASE_DIR}data/consul -ui -client=0.0.0.0 -bind=0.0.0.0 -join=${MASTER_IP} -dns-port=23346 -http-port=23345 -retry-join "[::1]:23341" -serf-wan-port=23342 &
+  fi
+}
+
+function install_sapspa_agent()
+{
+  # start sapspa_agent.py
+  echo "start sapspa_agent.py"
+  uwsgi --http 0.0.0.0:23310 --wsgi-file ${BASE_DIR}src/agent/sapspa_agent.py --callable app_dispatch --daemonize /var/log/uwsgi_sapspa_agent.log
+}
+
+function install_node_exporter()
+{
+  # download node_exporter
+  echo "download node_exporter"
+  wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz -O ${BASE_DIR}script/download/node_exporter.tar.gz
+  tar zxvf ${BASE_DIR}script/download/node_exporter.tar.gz /usr/local/bin
+  # start node_exporter
+  echo "start node_exporter"
+  nohup node_exporter --web.listen-address=":23311" &
+}
+
+function install_filebeat()
+{
+  # download ELK filebeat
+  wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-${CONSUL_VERSION}-linux-x86_64.tar.gz -O ${BASE_DIR}script/download/filebeat.tar.gz
+  tar zxvf ${BASE_DIR}script/download/filebeat.tar.gz ${BASE_DIR}script/download/
+  mv ${BASE_DIR}script/download/filebeat-${ELK_VERSION}-linux-x86_64 ${BASE_DIR}app/filebeat
+  # start filebeat
+  sed -i "s/localhost:23392/${MASTER_IP}:23392/g" ${BASE_DIR}etc/filebeat/filebeat.yml
+  nohup ${BASE_DIR}app/filebeat/filebeat -c ${BASE_DIR}etc/filebeat/filebeat.yml &
+}
+
+install_pyenv
+install_python3
+install_agent_requirements
+install_pyrfc
+install_consul
+install_sapspa_agent
+install_node_exporter
+install_filebeat
+
+echo "start agent done"
