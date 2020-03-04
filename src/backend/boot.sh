ae28ddfb9c5e0e907ecdffc780ea42a2f15e1056
redbearder <redbearder@gmail.com>
2020-03-04 13:08:34 +0800

    update migrations update grafana.db upgrade boot.sh

--- src/backend/boot.sh
+++ src/backend/boot.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # this script is used to boot a Docker container
 source venv/bin/activate
+
 while true; do
     venv/bin/flask db upgrade
     if [ "$?" = "0" ]; then
@@ -9,8 +10,5 @@ while true; do
     echo Deploy command failed, retrying in 5 secs...
     sleep 5
 done
-flask translate compile
-venv/bin/flask db init
-venv/bin/flask db migrate
-venv/bin/flask db upgrade
-exec venv/bin/gunicorn -b :22381 --access-logfile - --error-logfile - sapspa:app
+
+exec venv/bin/gunicorn -b :23381 --access-logfile - --error-logfile - sapspa:app
